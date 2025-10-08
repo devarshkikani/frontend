d1c98067a4082033ad6e753c3f1cecb3ffa0d73d
Devarsh Kikani <devarshkikani2904@gmail.com>
2025-10-08 17:30:07 +0530

    change the icon

--- src/App.js
+++ src/App.js
@@ -203,7 +203,7 @@ function App() {
         title={`Switch to ${theme === 'light' ? 'dark' : 'light'} mode`}
       >
         <span className="theme-icon">
-          {theme === 'light' ? '+++++' : '****'}
+          {theme === 'light' ? '🔆' : '🌙'}
         </span>
       </button>
 
